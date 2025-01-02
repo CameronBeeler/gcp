33ab28284a498bd050139ba8365b9b4ab5036128
Cam Beeler <cbeeler@sdgecontractor.com>
2025-01-02 14:42:54 -0600

    create-ts is now updated to manual workflow dispatch step

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable project_id {
+    description = "The project ID to deploy resources"
+    type        = string
+} 
+
+variable region {
+    description = "The region to deploy resources"
+    type        = string
+    default     = "us-central1"
+} 
