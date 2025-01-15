7a8b94f480dee3a9de67fc89bd24375ecd7407a3
Cameron Beeler <cameron.beeler@icloud.com>
2025-01-15 14:35:58 -0800

    corrected the tf vpc name

--- terraform/connectors.tf
+++ terraform/connectors.tf
@@ -3,12 +3,12 @@ resource "google_compute_global_address" "google_managed_services_range" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 20  # Adjust based on your requirements
-  network       = google_compute_network.custom_vpc.self_link
+  network       = google_compute_network.vpc_network.self_link
   description   = "Dynamically allocated IP Range for peer service connection networks."
 }
 # Network Access channels
 resource "google_service_networking_connection" "private_service_access" {
-  network                = google_compute_network.custom_vpc.self_link
+  network                = google_compute_network.vpc_network.self_link
   service                = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.google_managed_services_range.name]
 }
--- terraform/routes.tf
+++ terraform/routes.tf
@@ -2,7 +2,7 @@
 resource "google_compute_route" "route_to_internet_igw" {
   name                   = "route-to-internet-${var.environment}"
   description            = "Default route to the Internet."
-  network                = google_compute_network.fd_default_vpc.self_link
+  network                = google_compute_network.vpc_network.self_link
   dest_range             = "0.0.0.0/0"
   priority               = 1000
   next_hop_gateway       = "default-internet-gateway"
