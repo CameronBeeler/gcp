52e06918f91debbcac68a32fe3b97ce4d784f105
Cameron Beeler <cameron.beeler@icloud.com>
2025-01-20 23:20:59 -0800

    updated the providers to a more current version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.0.0"
+      version = ">= 6.13.0, < 7.0.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 6.13.0, < 7.0.0"
     }
   }
-  backend "gcs" {
-    bucket = "terraform-gcp-githubactions-state"
-    prefix = "hcl-sonorous-pact-445620-m2"
-  }
+  required_version = "~> 1.10.4"
 }
 
 provider "google" {
