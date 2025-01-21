278b1633242549a2a043145a1a662fbce8fb220b
Cameron Beeler <cameron.beeler@icloud.com>
2025-01-20 22:15:52 -0800

    added a private service access resource for the sqladmin google apis

--- terraform/connectors.tf
+++ terraform/connectors.tf
@@ -17,7 +17,7 @@ resource "google_project_service" "service_networking" {
 
 resource "google_project_service" "sqladmin_networking" {
   project    =  data.google_project.current.project_id
-  service = "sqladmin.googleapis.com"
+  service    = "sqladmin.googleapis.com"
 
   # Optionally wait for the API activation to propagate
   disable_on_destroy = false
@@ -31,6 +31,16 @@ resource "google_service_networking_connection" "private_service_access_service_
   depends_on               = [google_project_service.service_networking]
 }
 
+resource "google_service_networking_connection" "private_service_access_sqladmin_networking" {
+  network                  = google_compute_network.vpc_network.self_link
+  service                  = "sqladmin.googleapis.com"
+  reserved_peering_ranges  = [google_compute_global_address.google_managed_services_range.name]
+
+  depends_on               = [google_project_service.sqladmin_networking]
+}
+
+# Add a connector for serverless access from GCP Public to the VPC.
+## NOTE:  Network peering for the two service connections?  SQL at least.
 resource "google_project_service" "serverless_vpc_access" {
   project = var.project_id
   service = "vpcaccess.googleapis.com"
@@ -39,9 +49,6 @@ resource "google_project_service" "serverless_vpc_access" {
   disable_on_destroy = false
 }
 
-## NOTE
-# Add a connector for serverless access from GCP Public to the VPC.
-## NOTE:  Network peering for the two service connections?  SQL at least.
 resource "google_vpc_access_connector" "functions_connector" {
   name         = "functions-${var.region_abbreviations["${var.region}"]}-${substr(var.environment, 0,8)}"
   region       = var.region
