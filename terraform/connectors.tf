536b886b41f32ece6eb16121fd7bd64b67421cd7
Cameron Beeler <cameron.beeler@icloud.com>
2025-01-20 21:01:01 -0800

    updated the naming for the serverless vpc access connectors

--- terraform/connectors.tf
+++ terraform/connectors.tf
@@ -38,11 +38,12 @@ resource "google_project_service" "serverless_vpc_access" {
   # Optional: Prevent the service from being disabled during `terraform destroy`
   disable_on_destroy = false
 }
+
 ## NOTE
 # Add a connector for serverless access from GCP Public to the VPC.
 ## NOTE:  Network peering for the two service connections?  SQL at least.
-resource "google_vpc_access_connector" "connector_1" {
-  name         = "cloud-functions-${var.environment}"
+resource "google_vpc_access_connector" "functions_connector" {
+  name         = "cloud-functions-${var.region}-${var.environment}"
   region       = var.region
   network      = google_compute_network.vpc_network.name
   ip_cidr_range = "10.8.0.0/28" # IP range for connector traffic
@@ -50,8 +51,8 @@ resource "google_vpc_access_connector" "connector_1" {
   depends_on = [google_project_service.serverless_vpc_access]
 }
 
-resource "google_vpc_access_connector" "connector_2" {
-  name         = "fd-infra-vpc-connector-${var.environment}"
+resource "google_vpc_access_connector" "infra_connector" {
+  name         = "vpc-infra-connector-${var.region}-${var.environment}"
   region       = var.region
   network      = google_compute_network.vpc_network.name
   ip_cidr_range = "10.9.0.0/28" # IP range for connector traffic
