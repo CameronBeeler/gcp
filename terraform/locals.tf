be70053dbeada77e8233bc3c5b22167b79b2303d
Cameron Beeler <cameron.beeler@icloud.com>
2025-01-22 17:39:52 -0800

    removing dynamic variable from local declaration of firewall rule

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -12,7 +12,7 @@ locals{
 
   firewall_rules = {
     allow_internal_ingress_public = {
-      name           = "allow-all-ingress-internal-${var.environment}"
+      name           = "allow-all-ingress-internal"
       direction      = "INGRESS"
       priority       = 65534
       source_ranges  = ["10.128.0.0/9"]
