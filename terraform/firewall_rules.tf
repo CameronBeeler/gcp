d7c006058d2dae6b6b34edd14bd60e623fce2baa
Cameron Beeler <cameron.beeler@icloud.com>
2025-01-22 18:58:28 -0800

    firewall rules name corrected

---
+++ terraform/firewall_rules.tf
@@ -0,0 +1,16 @@
+resource "google_compute_firewall" "firewall_rules" {
+  for_each = local.firewall_rules
+
+  name    = "${each.value.name}-${var.environment}"
+  network = google_compute_network.vpc_network.name
+
+  direction = each.value.direction
+  priority  = each.value.priority
+
+  source_ranges = each.value.source_ranges
+
+  allow {
+    protocol = each.value.protocol
+    ports    = each.value.ports
+  }
+}
