7e440b8a7753c1bc13de9fda9351bd2d39e2ce56
Cameron Beeler <cameron.beeler@icloud.com>
2025-01-22 17:20:49 -0800

    removed reference to another api

--- terraform/connectors.tf
+++ terraform/connectors.tf
@@ -12,8 +12,6 @@ resource "google_service_networking_connection" "private_service_access_service_
   network                  = google_compute_network.vpc_network.self_link
   service                  = "servicenetworking.googleapis.com"
   reserved_peering_ranges  = [google_compute_global_address.google_managed_services_range.name]
-
-  depends_on               = [google_project_service.service_networking]
 }
 
 # Add a connector for serverless access from GCP Public to the VPC.
